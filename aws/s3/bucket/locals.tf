ccf4539679769d75ea7c6b437d9bdde7d64f776e
Guillem Casas <gucasassi@gmail.com>
2024-03-04 23:41:57 +0100

    chore: add test directory to .gitignore

--- aws/s3/bucket/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  # Generate a name for the S3 bucket, optionally appending a random suffix
-  name = var.add_random_suffix ? "${var.bucket_name}-${random_id.bucket_suffix[0].dec}" : var.bucket_name
-}
--- aws/s3/bucket/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "bucket_id" {
-  description = "The unique identifier of the created S3 bucket."
-  value       = aws_s3_bucket.this.id
-}
